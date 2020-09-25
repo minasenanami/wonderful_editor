6356cffed70574f8a6547b0b67399655f11569d1
kamise <minase.nanamin@gmail.com>
2020-09-25 16:24:33 +0900

    記事詳細の実装

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -18,4 +18,32 @@ RSpec.describe "Api::V1::Articles", type: :request do
       expect(res[0]["user"].keys).to eq ["id", "name", "email"]
     end
   end
+
+  describe " GET /articles/:id " do
+    subject { get(api_v1_article_path(article_id)) }
+
+    context " 指定した id の記事が存在する場合 " do
+      let(:article_id) { article.id }
+      let(:article) { create(:article) }
+
+      it " 指定した記事の値を取得できる " do
+        subject
+        res = JSON.parse(response.body)
+        expect(response).to have_http_status(ok)
+        expect(res["id"]).to eq article.id
+        expect(res["title"]).to eq article.title
+        expect(res["body"]).to eq article.body
+        expect(res["updated_at"]).to be_present
+        expect(res["user"].keys).to eq ["id", "name", "email"]
+      end
+    end
+
+    context " 指定したidが存在しない時 " do
+      let(:article_id) { 1_000_000 }
+
+      it " 記事が見つからない " do
+        expect { subject }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+  end
 end
