ce1c60c9c9878e6ad9fa5d650d7194d62edeeb16
kamise <minase.nanamin@gmail.com>
2020-09-29 16:14:31 +0900

    フロントエンドとの繋ぎこみを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for "User", at: "auth"
@@ -6,4 +8,3 @@ Rails.application.routes.draw do
     end
   end
 end
-# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
