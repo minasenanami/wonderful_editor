2c5dc4a67d829df13648616d90440765b094cabd
kamise <minase.nanamin@gmail.com>
2020-09-30 15:28:06 +0900

    ログインAPIの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
   has_many :articles, dependent: :destroy
   has_many :article_likes, dependent: :destroy
